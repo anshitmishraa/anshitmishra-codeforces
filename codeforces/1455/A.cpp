47d8e62dc39de40a189e9dccfff8700cc3cbbae4
Anshit Mishra <anshitmishraa@gmail.com>
2021-01-09 21:55:00 +0530

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/103897941

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl ("\n")
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    fast
+	int t;
+	cin>>t;
+	while(t--){
+		string s;
+		cin>>s;
+			
+		cout<<(s.size())<<endl;
+
+	}
+	return 0;
+}
